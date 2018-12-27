3ef0dc009cb6cb5056552a616725fdd47342468c
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2018-12-27 13:30:45 -0800

    Update LocalUserOptions + CMakeDevelConfig.cmake.in

--- Source/LocalUserOptions.cmake
+++ Source/LocalUserOptions.cmake
@@ -23,8 +23,8 @@ set(_CMAKE_LIBRARY_UTILITIES
 foreach(_PACKAGE ${_CMAKE_LIBRARY_UTILITIES})
     if(NOT CMAKE_USE_SYSTEM_${_PACKAGE} AND ${_PACKAGE}_LIBRARY)
         list(APPEND _CMAKE_LIBRARIES ${${_PACKAGE}_LIBRARY})
-    endif(NOT CMAKE_USE_SYSTEM_${_PACKAGE} AND ${_PACKAGE}_LIBRARY)
-endforeach(_PACKAGE ${_CMAKE_LIBRARY_UTILITIES})
+    endif()
+endforeach()
 
 list(REMOVE_DUPLICATES _CMAKE_LIBRARIES)
 
@@ -43,15 +43,6 @@ set(_CTEST_LIBRARIES
     CTestLib
     ${CMAKE_CURL_LIBRARIES}
     ${CMAKE_XMLRPC_LIBRARIES}
-    #cmsys
-    #${CMAKE_EXPAT_LIBRARIES}
-    #${CMAKE_ZLIB_LIBRARIES}
-    #${CMAKE_TAR_LIBRARIES}
-    #${CMAKE_COMPRESS_LIBRARIES}
-    #${CMAKE_JSONCPP_LIBRARIES}
-    #${CMAKE_LIBUV_LIBRARIES}
-    #${CMAKE_LIBRHASH_LIBRARIES}
-    #${CMake_KWIML_LIBRARIES}
 )
 
 # check if built or using system
@@ -62,8 +53,8 @@ set(_CTEST_LIBRARY_UTILITIES
 foreach(_PACKAGE ${_CTEST_LIBRARY_UTILITIES})
     if(NOT CMAKE_USE_SYSTEM_${_PACKAGE} AND ${_PACKAGE}_LIBRARY)
         list(APPEND _CTEST_LIBRARIES ${${_PACKAGE}_LIBRARY})
-    endif(NOT CMAKE_USE_SYSTEM_${_PACKAGE} AND ${_PACKAGE}_LIBRARY)
-endforeach(_PACKAGE ${_CTEST_LIBRARY_UTILITIES})
+    endif()
+endforeach()
 
 list(REMOVE_DUPLICATES _CTEST_LIBRARIES)
 
@@ -92,8 +83,8 @@ set(_CPACK_LIBRARY_UTILITIES
 foreach(_PACKAGE ${_CPACK_LIBRARY_UTILITIES})
     if(NOT CMAKE_USE_SYSTEM_${_PACKAGE} AND ${_PACKAGE}_LIBRARY)
         list(APPEND _CPACK_LIBRARIES ${${_PACKAGE}_LIBRARY})
-    endif(NOT CMAKE_USE_SYSTEM_${_PACKAGE} AND ${_PACKAGE}_LIBRARY)
-endforeach(_PACKAGE ${_CPACK_LIBRARY_UTILITIES})
+    endif()
+endforeach()
 
 list(REMOVE_DUPLICATES _CPACK_LIBRARIES)
 
@@ -101,5 +92,27 @@ export(TARGETS ${_CPACK_LIBRARIES}
     FILE ${CMAKE_BINARY_DIR}/CPackLibBuild.cmake)
 
 set_property(GLOBAL PROPERTY CPACK_PROJECT_LIBRARIES ${_CPACK_LIBRARIES})
+
+################################################################################
+#
+#       INSTALLATION
+#
+################################################################################
+include(GNUInstallDirs)
+
+set(_INSTALL_LIBRARIES ${_CMAKE_LIBRARIES} ${_CTEST_LIBRARIES} ${_CPACK_LIBRARIES})
+list(REMOVE_DUPLICATES _INSTALL_LIBRARIES)
+install(TARGETS ${_INSTALL_LIBRARIES}
+    DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    EXPORT CMakeDevelDepends)
+install(EXPORT CMakeDevelDepends
+    DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake)
+
+configure_file(${PROJECT_SOURCE_DIR}/Templates/CMakeDevelConfig.cmake.in
+    ${PROJECT_BINARY_DIR}/CMakeDevelConfig.cmake @ONLY)
+
+message(STATUS "Output: ${PROJECT_BINARY_DIR}/CMakeDevelConfig.cmake")
+install(FILES ${PROJECT_BINARY_DIR}/CMakeDevelConfig.cmake
+    DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake)
 
 ################################################################################
